5a51c71025f28fe05f4c7b9d772d8c93d4256f24
Imtiaz Sayed <contactimtiaz@yahoo.com>
2013-07-16 02:19:13 +0000

    Added grader.js grader.js.orig checks.json

--- grader.js
+++ grader.js
@@ -1,58 +1,79 @@
 #!/usr/bin/env node
-/*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
 
 var fs = require('fs');
+var url = require('url');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
-    var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
+    var instr = infile.toString(),
+        uri = url.parse(instr);
+    
+    if (!uri.protocol) {
+        if(!fs.existsSync(instr)) {
+            console.log("%s does not exist. Exiting.", instr);
+            process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        }
+    } else if (!/^http(s)?/.test(uri.protocol)) {
+        console.log("%s Unknown protocol. Exiting.", uri.protocol);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
+    
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var loadHtmlFile = function(htmlfile, callback) {
+    var uri = url.parse(htmlfile);
+    
+    if (uri.protocol) {
+        restler.get(htmlfile).on('complete', function(result) {
+            if (result instanceof Error)  {
+                console.log('Error while loading remote file.');
+                return callback(result);
+            } 
+            
+            return callback(null, result);
+        });
+    } else {
+        return fs.readFile(htmlfile, callback);
+    }
 };
+
+var cheerioHtmlFile = function(htmlfile, callback) {
+    loadHtmlFile(htmlfile, function (err, content) {
+        if (err) {
+            return callback(err);
+        }
+        
+        callback(null, cheerio.load(content.toString()));
+    });
+}
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
+var checkHtmlFile = function(htmlfile, checksfile, callback) {
+    var $, checks, out, ii;
+    
+    cheerioHtmlFile(htmlfile, function (err, content) {
+        if (err) {
+            return callback(err);
+        }
+        
+        $ = content;
+        checks = loadChecks(checksfile).sort();
+        out = {};
+        for(ii in checks) {
+            var present = $(checks[ii]).length > 0;
+            out[checks[ii]] = present;
+        }
+        
+        callback(null, out);
+    });
 };
 
 var clone = function(fn) {
@@ -62,13 +83,23 @@ var clone = function(fn) {
 };
 
 if(require.main == module) {
+    var outJson;
+    
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        
+    checkHtmlFile(program.file, program.checks, function (err, checkJson) {
+        if (err) {
+            console.log(err.message);
+            process.exit(1);
+        }
+        
+        outJson = JSON.stringify(checkJson, null, 4);
+        
+        console.log(outJson);
+    });
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
